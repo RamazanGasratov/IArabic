5135fa54bd0a35d9838fbd4937017a7e2b3a82eb
RamazanIOS <gasratov99@bk.ru>
2024-01-13 15:36:58 +0300

    first commit

---
+++ IArabic/IArabic/IArabicApp.swift
@@ -0,0 +1,20 @@
+//
+//  IArabicApp.swift
+//  IArabic
+//
+//  Created by Ramazan Gasratov on 13.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct IArabicApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
