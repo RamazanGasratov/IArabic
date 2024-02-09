3c905513566c87c970652a4d27bc59b000ebd43f
RamazanIOS <gasratov99@bk.ru>
2024-02-09 23:36:04 +0300

    - add CoreData and Network

--- IArabic/IArabic/CardsWords/View/CardWordsItem.swift
+++ IArabic/IArabic/CardsWords/View/CardWordsItem.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct CardWordsItem: View {
     var textTitle: String?
     var textTranslate: String?
-    
-    var image: String?
+    var mainImage: Data?
+    var assImage: Data?
 
     var body: some View {
         VStack(spacing: 35) {
@@ -36,11 +37,11 @@ struct CardWordsItem: View {
     
         var imageMain: some View {
             VStack {
-                Image(image ?? "сын")
+                Image(uiImage: UIImage(data: mainImage ?? Data()) ?? UIImage())
                     .resizable()
                     .frame(width: 281, height: 208)
                     .clipShape(RoundedRectangle(cornerRadius: 10))
-                Image(image ?? "сын")
+                Image(uiImage: UIImage(data: assImage ?? Data()) ?? UIImage())
                     .resizable()
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                     .frame(width: 263, height: 183)
--- IArabic/IArabic/CardsWords/View/CardsWordsRow.swift
+++ IArabic/IArabic/CardsWords/View/CardsWordsRow.swift
@@ -8,121 +8,121 @@
 import SwiftUI
 
 
-class CardViewModel: ObservableObject {
-  @Published var cards = WordsData.getAllWords
-}
+//class CardViewModel: ObservableObject {
+//  @Published var cards = WordsData.getAllWords
+//}
+//
+//struct CardsWordsRow: View {
+//    @State private var screenWith: CGFloat = 0
+//    @State private var cardHeight: CGFloat = 0
+//    
+//    let widthScale = 0.80
+//    let cardAspectRatio = 1.75
+//    
+//    @ObservedObject var vm = CardViewModel()
+//    
+//    @State var activeCardIndex = 1
+//    @State var draggOffset: CGFloat = 1
+//    
+//    var body: some View {
+//   
+//        GeometryReader { reader in
+//            ZStack {
+//                ForEach(vm.cards.indices, id: \.self) { index in
+//                    
+//                    CardWordsItem(textTitle: vm.cards[index].wordArabic, textTranslate: vm.cards[index].wordTranslate, image: vm.cards[index].imageURL)
+//                    
+//                        .frame(width: screenWith * widthScale, height: cardHeight)
+//                        .background(Color.custom.white)
+//                        .overlay {
+//                            Color.white.opacity( 1 - cardScale(for: index, proportion: 0.3))
+//                        }
+//                        .cornerRadius(20)
+////                        .offset(x: cardOffset(for: index))
+////                        .scaleEffect(x: cardScale(for: index), y: cardScale(for: index))
+////                        .zIndex(-Double(index))
+////                        .gesture(
+////                        DragGesture()
+////                                .onChanged { value in
+////                                    self.draggOffset = value.translation.width
+////                                }.onEnded{ value in
+////                                    let threshold = screenWith * 0.2
+////                                    
+////                                    withAnimation {
+////                                        if value.translation.width < -threshold {
+////                                            activeCardIndex = min(activeCardIndex + 1, vm.cards.count - 1)
+////                                        } else if value.translation.width > threshold  {
+////                                            activeCardIndex = max(activeCardIndex - 1, 0)
+////                                        }
+////                                    }
+////                                    
+////                                    withAnimation {
+////                                        draggOffset = 0
+////                                    }
+////                                }
+////                        )
+////                    }
+//            
+////            .shadow(radius: 7)
+////            .onAppear {
+////                print("CardsWordsRow appears")
+////                screenWith = reader.size.width
+////                cardHeight = screenWith * widthScale * cardAspectRatio
+//            }
+////            .onDisappear {
+////                print("CardsWordsRow disappears")
+//            }
+//            
+////            .offset(x: 16, y: 30)
+//        }
+//    }
+//    
+//    func cardOffset(for index: Int) -> CGFloat {
+//        let adjustedIndex = index - activeCardIndex
+//        
+//        let cardSpacing: CGFloat = 60 / cardScale(for: index)
+//        let initalOffset = cardSpacing * CGFloat(adjustedIndex)
+//        
+//        let progress = min(abs(draggOffset)/(screenWith/2), 1)
+//        let maxCardMovemet = cardSpacing
+//        
+//        if adjustedIndex < 0 {
+//            if draggOffset > 0 && index == activeCardIndex - 1 {
+//               
+//                let distanceToMove = (initalOffset + screenWith) * progress
+//                
+//                return -screenWith + distanceToMove
+//            } else {
+//                return -screenWith
+//            }
+//        } else if index > activeCardIndex {
+//           let diastanceToMove = progress * maxCardMovemet
+//            return initalOffset - (draggOffset < 0 ? diastanceToMove : -diastanceToMove)
+//        } else {
+//            if draggOffset < 0 {
+//                return draggOffset
+//            } else {
+//                let diastanceToMove = maxCardMovemet * progress
+//                return initalOffset - (draggOffset < 0 ? diastanceToMove : -diastanceToMove)
+//            }
+//        }
+//    }
+//    
+//    func cardScale(for index: Int, proportion: CGFloat = 0.2) -> CGFloat {
+//        let adjustedIndex = index - activeCardIndex
+//        if index > activeCardIndex {
+//            let progress = min(abs(draggOffset)/(screenWith/2), 1)
+//            return 1 - proportion * CGFloat(adjustedIndex) + (draggOffset < 0 ? proportion * progress : -proportion * progress)
+//        }
+//        
+//        
+//       return 1
+//    }
+//}
 
-struct CardsWordsRow: View {
-    @State private var screenWith: CGFloat = 0
-    @State private var cardHeight: CGFloat = 0
-    
-    let widthScale = 0.80
-    let cardAspectRatio = 1.75
-    
-    @ObservedObject var vm = CardViewModel()
-    
-    @State var activeCardIndex = 1
-    @State var draggOffset: CGFloat = 1
-    
-    var body: some View {
-   
-        GeometryReader { reader in
-            ZStack {
-                ForEach(vm.cards.indices, id: \.self) { index in
-                    
-                    CardWordsItem(textTitle: vm.cards[index].wordArabic, textTranslate: vm.cards[index].wordTranslate, image: vm.cards[index].imageURL)
-                    
-                        .frame(width: screenWith * widthScale, height: cardHeight)
-                        .background(Color.custom.white)
-                        .overlay {
-                            Color.white.opacity( 1 - cardScale(for: index, proportion: 0.3))
-                        }
-                        .cornerRadius(20)
-//                        .offset(x: cardOffset(for: index))
-//                        .scaleEffect(x: cardScale(for: index), y: cardScale(for: index))
-//                        .zIndex(-Double(index))
-//                        .gesture(
-//                        DragGesture()
-//                                .onChanged { value in
-//                                    self.draggOffset = value.translation.width
-//                                }.onEnded{ value in
-//                                    let threshold = screenWith * 0.2
-//                                    
-//                                    withAnimation {
-//                                        if value.translation.width < -threshold {
-//                                            activeCardIndex = min(activeCardIndex + 1, vm.cards.count - 1)
-//                                        } else if value.translation.width > threshold  {
-//                                            activeCardIndex = max(activeCardIndex - 1, 0)
-//                                        }
-//                                    }
-//                                    
-//                                    withAnimation {
-//                                        draggOffset = 0
-//                                    }
-//                                }
-//                        )
-//                    }
-            
-//            .shadow(radius: 7)
-//            .onAppear {
-//                print("CardsWordsRow appears")
-//                screenWith = reader.size.width
-//                cardHeight = screenWith * widthScale * cardAspectRatio
-            }
-//            .onDisappear {
-//                print("CardsWordsRow disappears")
-            }
-            
-//            .offset(x: 16, y: 30)
-        }
-    }
-    
-    func cardOffset(for index: Int) -> CGFloat {
-        let adjustedIndex = index - activeCardIndex
-        
-        let cardSpacing: CGFloat = 60 / cardScale(for: index)
-        let initalOffset = cardSpacing * CGFloat(adjustedIndex)
-        
-        let progress = min(abs(draggOffset)/(screenWith/2), 1)
-        let maxCardMovemet = cardSpacing
-        
-        if adjustedIndex < 0 {
-            if draggOffset > 0 && index == activeCardIndex - 1 {
-               
-                let distanceToMove = (initalOffset + screenWith) * progress
-                
-                return -screenWith + distanceToMove
-            } else {
-                return -screenWith
-            }
-        } else if index > activeCardIndex {
-           let diastanceToMove = progress * maxCardMovemet
-            return initalOffset - (draggOffset < 0 ? diastanceToMove : -diastanceToMove)
-        } else {
-            if draggOffset < 0 {
-                return draggOffset
-            } else {
-                let diastanceToMove = maxCardMovemet * progress
-                return initalOffset - (draggOffset < 0 ? diastanceToMove : -diastanceToMove)
-            }
-        }
-    }
-    
-    func cardScale(for index: Int, proportion: CGFloat = 0.2) -> CGFloat {
-        let adjustedIndex = index - activeCardIndex
-        if index > activeCardIndex {
-            let progress = min(abs(draggOffset)/(screenWith/2), 1)
-            return 1 - proportion * CGFloat(adjustedIndex) + (draggOffset < 0 ? proportion * progress : -proportion * progress)
-        }
-        
-        
-       return 1
-    }
-}
-
-#Preview {
-    CardWordsView()
-}
+//#Preview {
+//    CardWordsView()
+//}
 
 
 struct CardsWordsRow2: View {
@@ -131,13 +131,15 @@ struct CardsWordsRow2: View {
     
     @State var words = WordsData.getAllWords
     
+    @ObservedObject var dataWords: CoreDataViewModel
+    
     var body: some View {
         VStack(spacing: 15) {
-            SnapCarousel(index: $currentIndex, items: words) { word in
+            SnapCarousel(index: $currentIndex, items: dataWords.saveEntities) { word in
                 GeometryReader { proxy in
                     let size = proxy.size
                     
-                    CardWordsItem(textTitle: word.wordTranslate, textTranslate: word.wordArabic, image: word.imageURL)
+                    CardWordsItem(textTitle: word.translate, textTranslate: word.title, mainImage: word.imageMain, assImage: word.associatImage)
                         .frame(width: size.width)
                         .cornerRadius(10)
                 }
@@ -145,9 +147,6 @@ struct CardsWordsRow2: View {
             .padding(.vertical, 40)
         }
         .frame(maxHeight: .infinity, alignment: .top)
-        .onAppear {
-            words
-        }
     }
 }
 
